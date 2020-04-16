ffda206064c825bd4c0efb4667ef2a139fd19d15
mryode <mryodedev@gmail.com>
2020-04-16 16:54:37 +0300

    section images minified

--- client/src/redux/directory/directoryData.js
+++ client/src/redux/directory/directoryData.js
@@ -1,45 +1,33 @@
-const MINIFIED = true;
-
 export const sections = [
   {
     id: '123-111-111-111',
     title: 'drums',
-    imageUrl: `${process.env.PUBLIC_URL}/products/drums/sectionImg${
-      MINIFIED ? '-min' : ''
-    }.jpg`,
+    imageUrl: `${process.env.PUBLIC_URL}/products/drums/sectionImg-min.jpg`,
     linkUrl: 'shop/drums',
   },
   {
     id: '123-111-111-222',
     title: 'pianos',
-    imageUrl: `${process.env.PUBLIC_URL}/products/pianos/sectionImg${
-      MINIFIED ? '-min' : ''
-    }.jpg`,
+    imageUrl: `${process.env.PUBLIC_URL}/products/pianos/sectionImg-min.jpg`,
     linkUrl: 'shop/pianos',
   },
   {
     id: '123-111-111-333',
     title: 'audio',
-    imageUrl: `${process.env.PUBLIC_URL}/products/audio/sectionImg${
-      MINIFIED ? '-min' : ''
-    }.jpg`,
+    imageUrl: `${process.env.PUBLIC_URL}/products/audio/sectionImg-min.jpg`,
     linkUrl: 'shop/audio',
   },
   {
     id: '123-111-111-444',
     title: 'basses',
-    imageUrl: `${process.env.PUBLIC_URL}/products/basses/sectionImg${
-      MINIFIED ? '-min' : ''
-    }.jpg`,
+    imageUrl: `${process.env.PUBLIC_URL}/products/basses/sectionImg-min.jpg`,
     linkUrl: 'shop/basses',
     size: 'large',
   },
   {
     id: '123-111-111-555',
     title: 'guitars',
-    imageUrl: `${process.env.PUBLIC_URL}/products/guitars/sectionImg${
-      MINIFIED ? '-min' : ''
-    }.jpg`,
+    imageUrl: `${process.env.PUBLIC_URL}/products/guitars/sectionImg-min.jpg`,
     linkUrl: 'shop/guitars',
     size: 'large',
   },
